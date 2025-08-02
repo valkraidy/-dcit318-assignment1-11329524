fa30add2c4ddf66622324abba4d5f882b52c8e3c
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2025-08-01 16:23:22 -1200

    Ticket project

---
+++ Ticket Price Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ticket Price Calculator");
+        Console.WriteLine("Enter your age");
+        int age = Convert.ToInt32(Console.ReadLine());
+        double dicounted_price = 7.00;
+        double ticket_price = 10.00;
+        try
+        {
+
+            if (age >= 65 ||  age <= 12)
+            {
+                Console.WriteLine("Your age is " + age);
+                Console.WriteLine($"Ticket Price:{dicounted_price}");
+            }
+            else
+            {
+                Console.WriteLine("Your age is " + age);
+                Console.WriteLine($"Ticket Price:{ticket_price}");
+            }
+        }
+        catch(FormatException)
+        {
+            Console.WriteLine("Error: Invalid input. Please enter a valid integer");
+
+        }
+    }
+}
